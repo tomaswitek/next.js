c39bab8264af066b5da595096b58728dd9ce5edf
Tomáš Witek <tomas@witek.cz>
2018-02-06 17:51:45 +0100

    Working example of shared component in with-zones

--- examples/with-zones/packages/components/src/Header.js
+++ examples/with-zones/packages/components/src/Header.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React from "react";
 
-export default componentName;
 const Header = () => {
   return <div>Header</div>;
 };
--- examples/with-zones/packages/components/src/index.js
+++ examples/with-zones/packages/components/src/index.js
@@ -1,5 +1,3 @@
-import Header from "./header";
+import Header from "./Header";
 
-export default {
-  Header
-};
+export { Header };
